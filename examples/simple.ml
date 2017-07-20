e505744155ade487eb555ee5bac7adcf8cdeed2e
Travis Brady <travis.brady@gmail.com>
2017-07-20 09:19:37 -0500

    checkpoint

--- examples/simple.ml
+++ examples/simple.ml
@@ -1,7 +1,4 @@
 (*
-  (c_flags (-I/usr/local/include))
-  (c_library_flags (-ldynamic -lreactor -fPIC))
-  (library_flags (-linkall))
   *)
 let () =
     let r = Reactor.C.reactor_core_construct () in
